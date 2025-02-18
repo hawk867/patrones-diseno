0716980170d95b69aecf6f3770e1f283cd82c620
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2025-02-17 19:50:53 -0500

    patron factory method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,78 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Burger {
+    prepare(): void;
+}
+
+class ChickenBurger implements Burger {
+    prepare(): void {
+        console.log('Preparing %cChicken Burger', COLORS.yellow);
+    }
+}
+
+class BeefBurger implements Burger {
+    prepare(): void {
+        console.log('Preparing %cBeef Burger', COLORS.red);
+    }
+}
+
+class BeanBurger implements Burger {
+    prepare(): void {
+        console.log('Preparing %cBean Burger', COLORS.green);
+    }
+}
+
+// una clase abstracta sirve para definir el esqueleto de otra clase
+abstract class Restaurant {
+
+    abstract createBurger(): Burger;
+
+    orderBurger(): void {
+        const burger = this.createBurger();
+        burger.prepare();
+    }
+}
+
+class ChickenRestaurant extends Restaurant {
+    override createBurger(): Burger {
+        return new ChickenBurger();
+    }
+}
+
+class BeefRestaurant extends Restaurant {
+    override createBurger(): Burger {
+        return new BeefBurger();
+    }
+}
+
+class BeanRestaurant extends Restaurant {
+    override createBurger(): Burger {
+        return new BeanBurger();
+    }
+}
+
+function main() {
+    let restaurant: Restaurant;
+    const burgerType = prompt('What burger do you want? (chicken/beef/bean)');
+
+    switch (burgerType) {
+        case 'chicken':
+            restaurant = new ChickenRestaurant();
+            break;
+        case 'beef':
+            restaurant = new BeefRestaurant();
+            break;
+        case 'bean':
+            restaurant = new BeanRestaurant();
+            break;
+        default:
+            throw new Error('Invalid burger type');
+    }
+    restaurant.orderBurger();
+}
+
+main();

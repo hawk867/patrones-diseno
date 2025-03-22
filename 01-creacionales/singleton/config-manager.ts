e631d6adb322a47315ca1c52f002d0802984a417
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2025-03-22 15:20:53 -0500

    patron singleton forma alternativa de implementarlo

---
+++ 01-creacionales/singleton/config-manager.ts
@@ -0,0 +1,18 @@
+
+
+class ConfigManager {
+
+    private config: Record<string, string> = {};
+
+    setConfig(key: string, value: string) {
+        this.config[key] = value;
+    }
+    getConfig(key: string): string | null {
+        return this.config[key];
+    }
+    getAllConfig(): Record<string, string> {
+        return {...this.config};
+    }
+}
+
+export const configManager = new ConfigManager();

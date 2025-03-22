f5e7014379427d98a25b717e75371c943bd9229f
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2025-03-22 15:20:05 -0500

    patron singleton solucion

--- 01-creacionales/06.2-singleton.ts
+++ 01-creacionales/06.2-singleton.ts
@@ -20,17 +20,33 @@ class DatabaseConnection {
   // Método estático para obtener la instancia única
   public static getInstance(): DatabaseConnection {
     // Completar: implementar el patrón Singleton
-    throw new Error('Method not implemented.');
+    if ( !DatabaseConnection.instance) {
+      DatabaseConnection.instance = new DatabaseConnection();
+      console.log('%cSe ha creado una instancia de la base de datos', COLORS.green);
+    }
+    return DatabaseConnection.instance;
   }
 
   // Método para conectar a la base de datos
   public connect(): void {
     // Completar: si no está conectado, mostrar mensaje de conexión
+    if ( !this.connected ) {
+      this.connected = true;
+      console.log('%cConexion a la base de datos establecida', COLORS.green);
+      return;
+    }
+    console.log('%cYa existe una conexion activa', COLORS.red);    
   }
 
   // Método para desconectar de la base de datos
   public disconnect(): void {
     // Completar: desconectar y mostrar mensaje de desconexión
+    if ( this.connected ) {
+      this.connected = false;
+      console.log('%cConexion a la base de datos cerrada', COLORS.blue);
+      return;
+    }
+    console.log('%cNo hay conexion a la base de datos para cerrar', COLORS.red);
   }
 }
 

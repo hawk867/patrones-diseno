f82f5de5f561dcc96fa57a309f7431ed8aa782db
Daniel Esteban Cepeda Rojas <whisper1831@hotmail.com>
2025-03-23 13:47:26 -0500

    Patron adapter implementación

--- 02-estructurales/adapter-files/logger-adapter.ts
+++ 02-estructurales/adapter-files/logger-adapter.ts
@@ -1,3 +1,33 @@
 import { Logger } from 'jsr:@deno-library/logger';
+import { COLORS } from '../../helpers/colors.ts';
 
-// TODO: Implementar el LoggerAdapter
+interface ILoggerAdapter {
+    file: string;
+
+    writeLog: ( msg: string ) => void;
+    writeError: ( msg: string ) => void;
+    writeWarn: ( msg: string ) => void;
+}
+
+export class DenoLoggerAdapter implements ILoggerAdapter {
+        
+    public file: string;
+    private logger = new Logger();
+
+    constructor( file: string ) {
+        this.file = file;
+    }
+
+    writeLog( msg: string ): void {
+        this.logger.info(`[${ this.file } Log] %c${ msg }`, COLORS.blue);
+    }
+
+    writeError( msg: string ) {
+        this.logger.error(`[${ this.file } Error] %c${ msg }`, COLORS.red);
+    }
+
+    writeWarn( msg: string ) {
+        this.logger.warn(`[${ this.file } Warning] %c${ msg }`, COLORS.yellow);
+    }
+
+}
